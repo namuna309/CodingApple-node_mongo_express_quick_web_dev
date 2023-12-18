5bb074bae6bc3518e76d6234b8dc1ef4dd8f72ad
namuna309 <nyah309@gmail.com>
2023-12-18 13:23:12 +0900

    웹페이지에 디자인 넣으려면

--- forum/server.js
+++ forum/server.js
@@ -5,6 +5,20 @@ app.listen(8080, () => {
     console.log('http://localhost:8080 에서 서버 실행중')
 })
 
-app.get('/', (요청, 응답) => {
-  응답.send('반갑다')
+app.get('/', (req, res) => {
+    // res.send('반갑다');
+    res.sendFile(__dirname + '/index.html');
+})
+
+app.get('/news', (req, res) => {
+  res.send('오늘 비옴');
 }) 
+
+app.get('/shop', (req, res) => {
+    res.send('쇼핑페이지임');
+})
+
+// 웹페이지에 디자인 넣으려면: 숙제
+app.get('/about', (req, res) => {
+    res.sendFile(__dirname + '/about.html');
+})
