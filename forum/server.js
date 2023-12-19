f6297250bb5d47e272324f4b944e89c75898b898
namuna309 <nyah309@gmail.com>
2023-12-19 15:25:00 +0900

    웹페이지에 DB데이터 꽂기 (EJS, 서버사이드 렌더링) + 숙제

--- forum/server.js
+++ forum/server.js
@@ -1,46 +1,54 @@
 const express = require('express')
 const app = express()
+require('dotenv').config();
+db_key = process.env.MONGODB_PW;
 
+// settings
 app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'ejs');
+
 
 app.listen(8080, () => {
-    console.log('http://localhost:8080 에서 서버 실행중')
+  console.log('http://localhost:8080 에서 서버 실행중')
 })
 
 app.get('/', (req, res) => {
-    // res.send('반갑다');
-    res.sendFile(__dirname + '/index.html');
+  // res.send('반갑다');
+  res.sendFile(__dirname + '/index.html');
 })
 
 
 
 app.get('/shop', (req, res) => {
-    res.send('쇼핑페이지임');
+  res.send('쇼핑페이지임');
 })
 
 // 웹페이지에 디자인 넣으려면: 숙제
 app.get('/about', (req, res) => {
-    res.sendFile(__dirname + '/about.html');
+  res.sendFile(__dirname + '/about.html');
 })
 
 const { MongoClient } = require('mongodb')
 
 let db
-const url = 'mongodb+srv://nyah309:password@cluster0.emzshpb.mongodb.net/?retryWrites=true&w=majority'
-new MongoClient(url).connect().then((client)=>{
+const url = `mongodb+srv://nyah309:${db_key}@cluster0.emzshpb.mongodb.net/?retryWrites=true&w=majority`
+new MongoClient(url).connect().then((client) => {
   console.log('DB연결성공')
   db = client.db('forum')
-}).catch((err)=>{
+}).catch((err) => {
   console.log(err)
 })
 
 app.get('/news', (req, res) => {
-    db.collection('post').insertOne({title: '어쩌구'});
-    res.send('오늘 비옴');
-  }) 
+  db.collection('post').insertOne({ title: '어쩌구' });
+  res.send('오늘 비옴');
+})
 
 app.get('/list', async (req, res) => {
-    let result = await db.collection('post').find().toArray();
-    console.log(result[0]);
-    res.send('DB에 있던 게시물');
-  }) 
+  let result = await db.collection('post').find().toArray();
+  res.render('list.ejs', { post: result });
+})
+
+app.get('/time', (req, res) => {
+  res.render('time.ejs', {time: new Date()});
+})
