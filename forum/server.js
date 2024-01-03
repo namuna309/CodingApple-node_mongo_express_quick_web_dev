9b74f1656bbd686f92da2d8f58f7b94793992ed9
namuna309 <nyah309@gmail.com>
2024-01-03 09:10:43 +0900

    채팅기능 만들기 (Socket.io)

--- forum/server.js
+++ forum/server.js
@@ -13,6 +13,11 @@ const { S3Client } = require('@aws-sdk/client-s3')
 const multer = require('multer')
 const multerS3 = require('multer-s3')
 
+const { createServer } = require('http')
+const { Server } = require('socket.io')
+const server = createServer(app)
+const io = new Server(server)
+
 
 require('dotenv').config();
 const db_key = process.env.MONGODB_PW;
@@ -82,8 +87,17 @@ const upload = multer({
   })
 })
 
-app.listen(8080, () => {
-  console.log('http://localhost:8080 에서 서버 실행중')
+let connectDB = require('./routes/database')
+let db
+
+connectDB.then((client) => {
+  console.log('DB연결성공')
+  db = client.db('forum')
+  server.listen(8080, () => {
+    console.log('http://localhost:8080 에서 서버 실행중')
+  });
+}).catch((err) => {
+  console.log(err)
 })
 
 app.use('/shop', require('./routes/shop.js') );
@@ -104,15 +118,7 @@ app.get('/about', (req, res) => {
   res.sendFile(__dirname + '/about.html');
 })
 
-let connectDB = require('./routes/database')
-let db
 
-connectDB.then((client) => {
-  console.log('DB연결성공')
-  db = client.db('forum')
-}).catch((err) => {
-  console.log(err)
-})
 
 app.get('/news', (req, res) => {
   db.collection('post').insertOne({ title: '어쩌구' });
@@ -158,7 +164,7 @@ app.post('/add', upload.single('img1'), async (req, res)=>{
     }
    
   }
-}) 
+})  
 
 app.get('/detail/:id', async (req, res) => {
   try {
@@ -283,3 +289,44 @@ app.get('/search', async (req, res) => {
   let result = await db.collection('post').find( {title : {$regex: reqData} } ).toArray();
   res.render('search.ejs', { post: result});
 })
+
+
+// 채팅기능
+// 1. 글마다 채팅버튼 누르면 채팅방 생성
+// 2. 내가 속한 채팅방 목록 페이지 있음
+// 3. 채팅방 누르면 채팅방 상세페이지 보여줌
+// 4. 메세지 전송시 상대에게 전달/DB에 저장
+
+app.get('/chat/request', async (req, res)=>{
+  await db.collection('chatroom').insertOne({
+    member : [req.user._id, new ObjectId(req.query.writerId)],
+    date : new Date()
+  })
+  res.redirect('채팅방목록페이지')
+})
+
+app.get('/chat/list', async (req, res)=>{
+  let result = await db.collection('chatroom').find({ member : req.user._id }).toArray()
+  res.render('chatList.ejs', {글목록 : result})
+}) 
+
+app.get('/chat/detail', async (req, res)=>{
+  res.render('chatDetail.ejs')
+}) 
+
+io.on('connection', (socket) => {
+  console.log('어떤놈이 웹소켓 연결함');
+  socket.on('age', (data) => {
+    console.log('유저가 보낸거 ', data);
+    io.emit('name', 'kim');
+  })
+
+  socket.on('ask-join', (data) => {
+    socket.join(data);
+  })
+
+  socket.on('message', (data) => {
+    io.to(data.room).emit('broadcast', data.msg);
+  })
+})
+
