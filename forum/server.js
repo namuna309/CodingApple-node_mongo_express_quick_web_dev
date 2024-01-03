c4402af10c7e14cb052edef2560453e2003c67d9
namuna309 <nyah309@gmail.com>
2024-01-03 09:36:24 +0900

    실시간 데이터 푸시 - Server sent events + Change Stream

--- forum/server.js
+++ forum/server.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const app = express()
-const { ObjectId, MongoClient } = require('mongodb') 
+const { ObjectId, MongoClient } = require('mongodb')
 const methodOverride = require('method-override');
 
 const session = require('express-session')
 const passport = require('passport')
 const LocalStrategy = require('passport-local')
-const bcrypt = require('bcrypt') 
+const bcrypt = require('bcrypt')
 const MongoStore = require('connect-mongo')
 
 const { S3Client } = require('@aws-sdk/client-s3')
@@ -27,23 +27,23 @@ app.use(methodOverride('_method'));
 app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
 app.use(express.json())
-app.use(express.urlencoded({extended:true})) 
+app.use(express.urlencoded({ extended: true }))
 app.use(passport.initialize())
 app.use(session({
   secret: process.env.SESSION_SECRET,
-  resave : false,
-  saveUninitialized : false,
-  cookie : { maxAge : 60 * 60 * 1000 },
+  resave: false,
+  saveUninitialized: false,
+  cookie: { maxAge: 60 * 60 * 1000 },
   store: MongoStore.create({
-    mongoUrl : `mongodb+srv://nyah309:${db_key}@cluster0.emzshpb.mongodb.net/?retryWrites=true&w=majority`,
+    mongoUrl: `mongodb+srv://nyah309:${db_key}@cluster0.emzshpb.mongodb.net/?retryWrites=true&w=majority`,
     dbName: 'forum',
   })
 }))
-app.use(passport.session()) 
+app.use(passport.session())
 
 // passport library setting
 passport.use(new LocalStrategy(async (inputId, inputPw, cb) => {
-  let result = await db.collection('user').findOne({ username : inputId})
+  let result = await db.collection('user').findOne({ username: inputId })
   if (!result) {
     return cb(null, false, { message: '아이디 DB에 없음' })
   }
@@ -61,7 +61,7 @@ passport.serializeUser((user, done) => {
 })
 
 passport.deserializeUser(async (user, done) => {
-  let result = await db.collection('user').findOne({_id : new ObjectId(user.id) })
+  let result = await db.collection('user').findOne({ _id: new ObjectId(user.id) })
   delete result.password
   process.nextTick(() => {
     return done(null, result)
@@ -70,10 +70,10 @@ passport.deserializeUser(async (user, done) => {
 
 // AWS S3 access
 const s3 = new S3Client({
-  region : 'ap-northeast-2',
-  credentials : {
-      accessKeyId : process.env.AWS_ACCESS_KEY,
-      secretAccessKey : process.env.AWS_SECRET_KEY
+  region: 'ap-northeast-2',
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY,
+    secretAccessKey: process.env.AWS_SECRET_KEY
   }
 })
 
@@ -89,18 +89,25 @@ const upload = multer({
 
 let connectDB = require('./routes/database')
 let db
-
+let changeStream;
 connectDB.then((client) => {
+  let cond = [
+    { $match : {operationType: 'insert'}}
+  ]
+
+  
   console.log('DB연결성공')
   db = client.db('forum')
   server.listen(8080, () => {
     console.log('http://localhost:8080 에서 서버 실행중')
+    changeStream = db.collection('post').watch(cond);
   });
+  
 }).catch((err) => {
   console.log(err)
 })
 
-app.use('/shop', require('./routes/shop.js') );
+app.use('/shop', require('./routes/shop.js'));
 
 app.get('/', (req, res) => {
   // res.send('반갑다');
@@ -134,18 +141,18 @@ function logTime(res, req, next) {
 app.use('/list', logTime);
 
 app.get('/list', async (req, res) => {
-  let result = await db.collection('post').find().limit(5).toArray();  res.render('list.ejs', { post: result });
+  let result = await db.collection('post').find().limit(5).toArray(); res.render('list.ejs', { post: result });
 })
 
 app.get('/time', (req, res) => {
-  res.render('time.ejs', {time: new Date()});
+  res.render('time.ejs', { time: new Date() });
 })
 
 app.get('/write', (req, res) => {
   res.render('write.ejs');
 })
 
-app.post('/add', upload.single('img1'), async (req, res)=>{
+app.post('/add', upload.single('img1'), async (req, res) => {
   let data = req.body;
 
   if (data.title == '') {
@@ -154,29 +161,29 @@ app.post('/add', upload.single('img1'), async (req, res)=>{
     res.send('내용?');
   } else {
     try {
-      await db.collection('post').insertOne({ title: data.title, content: data.content, img: req.file.location});
+      await db.collection('post').insertOne({ title: data.title, content: data.content, img: req.file.location });
       await db.collection('post').find().toArray();
       console.log('입력 성공');
       res.redirect('/list');
-    } catch(e) {
+    } catch (e) {
       console.log('db 입력 실패');
       res.status(500).send(e);
     }
-   
+
   }
-})  
+})
 
 app.get('/detail/:id', async (req, res) => {
   try {
     let objId = req.params;
-    let result = await db.collection('post').findOne({_id: new ObjectId(objId)})
+    let result = await db.collection('post').findOne({ _id: new ObjectId(objId) })
     console.log(result);
     if (result == null) {
       응답.status(400).send('그런 글 없음')
     } else {
-      res.render('detail.ejs', {post: result});
+      res.render('detail.ejs', { post: result });
     }
-  } catch(e) {
+  } catch (e) {
     console.log(e);
     res.status(400).send('이상한 url 입력함');
   }
@@ -184,8 +191,8 @@ app.get('/detail/:id', async (req, res) => {
 
 app.get('/edit/:id', async (req, res) => {
   let objId = req.params;
-  let result = await db.collection('post').findOne({_id: new ObjectId(objId)});
-  res.render('edit.ejs', {post: result});
+  let result = await db.collection('post').findOne({ _id: new ObjectId(objId) });
+  res.render('edit.ejs', { post: result });
 
 })
 
@@ -201,10 +208,10 @@ app.get('/edit/:id', async (req, res) => {
 //     res.status(400).send(err);
 //   }
 // })
-app.put('/edit', async (req, res)=>{
+app.put('/edit', async (req, res) => {
   let data = req.body;
   try {
-    await db.collection('post').updateOne({_id: new ObjectId(data.id)}, {$set: { title : data.title, content: data.content }});
+    await db.collection('post').updateOne({ _id: new ObjectId(data.id) }, { $set: { title: data.title, content: data.content } });
     console.log('수정 성공');
     res.redirect(`/detail/${data.id}`);
   } catch (err) {
@@ -217,17 +224,17 @@ app.post('/abc', async (req, res) => {
   console.log(req.body);
 })
 
-app.delete('/delete', async(req, res) => {
+app.delete('/delete', async (req, res) => {
   let post = req.query.id;
   console.log(post);
-  try{
-    await db.collection('post').deleteOne({_id: new ObjectId(post)});
+  try {
+    await db.collection('post').deleteOne({ _id: new ObjectId(post) });
     console.log('삭제 완료')
     res.send('삭제 완료');
-  } catch(err) {
+  } catch (err) {
     console.log(err);
   }
-  
+
 })
 
 // app.get('/list/:page', async (req, res) => {
@@ -237,7 +244,7 @@ app.delete('/delete', async(req, res) => {
 // })
 
 app.get('/list/next/:page', async (req, res) => {
-  let result = await db.collection('post').find({_id : {$gt: new ObjectId(req.params.page)}}).limit(5).toArray();
+  let result = await db.collection('post').find({ _id: { $gt: new ObjectId(req.params.page) } }).limit(5).toArray();
   res.render('list.ejs', { post: result });
 })
 
@@ -245,16 +252,16 @@ app.get('/list/next/:page', async (req, res) => {
 function isBlank(req, res, next) {
   let id = req.body.username;
   let pw = req.body.password;
-  if((id == '') || (pw == '')) {
+  if ((id == '') || (pw == '')) {
     res.send('그러지마세요');
   } else {
     next();
   }
 }
 
-app.get('/login', (req, res)=>{
+app.get('/login', (req, res) => {
   res.render('login.ejs')
-}) 
+})
 
 app.post('/login', async (req, res, next) => {
   // 제출한아이디/비번이 DB에 있는거랑 일치하는지 확인하고 세션생성
@@ -265,20 +272,20 @@ app.post('/login', async (req, res, next) => {
       if (err) return next(err)
       res.redirect('/')
     })
-})(req, res, next)
+  })(req, res, next)
 
-}) 
+})
 
 // 회원가입
-app.get('/register', (req, res)=>{
+app.get('/register', (req, res) => {
   res.render('register.ejs')
 })
 
 app.post('/register', async (req, res) => {
-  let hash = await bcrypt.hash(req.body.password, 10) 
+  let hash = await bcrypt.hash(req.body.password, 10)
   await db.collection('user').insertOne({
-    username : req.body.username,
-    password : hash
+    username: req.body.username,
+    password: hash
   })
   res.redirect('/')
 })
@@ -286,8 +293,8 @@ app.post('/register', async (req, res) => {
 
 app.get('/search', async (req, res) => {
   let reqData = req.query.val;
-  let result = await db.collection('post').find( {title : {$regex: reqData} } ).toArray();
-  res.render('search.ejs', { post: result});
+  let result = await db.collection('post').find({ title: { $regex: reqData } }).toArray();
+  res.render('search.ejs', { post: result });
 })
 
 
@@ -297,22 +304,22 @@ app.get('/search', async (req, res) => {
 // 3. 채팅방 누르면 채팅방 상세페이지 보여줌
 // 4. 메세지 전송시 상대에게 전달/DB에 저장
 
-app.get('/chat/request', async (req, res)=>{
+app.get('/chat/request', async (req, res) => {
   await db.collection('chatroom').insertOne({
-    member : [req.user._id, new ObjectId(req.query.writerId)],
-    date : new Date()
+    member: [req.user._id, new ObjectId(req.query.writerId)],
+    date: new Date()
   })
   res.redirect('채팅방목록페이지')
 })
 
-app.get('/chat/list', async (req, res)=>{
-  let result = await db.collection('chatroom').find({ member : req.user._id }).toArray()
-  res.render('chatList.ejs', {글목록 : result})
-}) 
+app.get('/chat/list', async (req, res) => {
+  let result = await db.collection('chatroom').find({ member: req.user._id }).toArray()
+  res.render('chatList.ejs', { 글목록: result })
+})
 
-app.get('/chat/detail', async (req, res)=>{
+app.get('/chat/detail', async (req, res) => {
   res.render('chatDetail.ejs')
-}) 
+})
 
 io.on('connection', (socket) => {
   console.log('어떤놈이 웹소켓 연결함');
@@ -330,3 +337,20 @@ io.on('connection', (socket) => {
   })
 })
 
+app.get('/stream/list', (req, res) => {
+  res.writeHead(200, {
+    "Connection": "keep-alive",
+    "Content-Type": "text/event-stream",
+    "Cache-Control": "no-cache",
+  })
+
+  
+  
+  
+  changeStream.on('change', (result) => {
+    console.log(result);
+    res.write('event: msg\n');
+    res.write(`data: ${JSON.stringify(result.fullDocument)}\n\n`);
+  })
+
+})
